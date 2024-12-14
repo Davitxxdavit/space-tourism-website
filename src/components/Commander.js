01cfd15d8b1ec4fe722574dda90d167298d861f6
zaden <davitberidze1510@gmail.com>
2024-12-15 02:44:16 +0400

    second

--- src/components/Commander.js
+++ src/components/Commander.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Commander.css';
-import commanderimg from '../assets/crew/image-douglas-hurley.png';
+
 const Commander = () => {
   return (
     <div className='commander'>
